3b0bedc7070d6bf9f5f24ffd3b81d45d546e4128
MohitCode17 <mohit.codes17@gmail.com>
2024-10-11 21:50:36 +0530

    add cors config & default middleware

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,31 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import { errorMiddleware } from "./middlewares/error.js";
+import { config } from "./config/env.config.js";
 
 const app = express();
 
 // CORS CONFIG
+app.use(
+  cors({
+    origin: config.FRONTEND_URL,
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
 
 // DEFAULT MIDDLEWARE
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // TESTING ROUTE
 app.get("/", (req, res) => {
