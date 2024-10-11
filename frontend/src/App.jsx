1d4eb9d5589ed0ddefc67e2862e63c8e496c4cde
MohitCode17 <mohit.codes17@gmail.com>
2024-10-11 21:23:07 +0530

    initialize frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-green-500 font-bold text-3xl">
+        Let's build Luxora Ecommerce Platform.
+      </h1>
+      <Button>Let's start</Button>
+    </div>
+  );
+};
+
+export default App;
