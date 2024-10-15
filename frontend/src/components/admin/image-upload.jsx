c5d2906daeb8b595d810e7c836206d28f7d99724
MohitCode17 <mohit.codes17@gmail.com>
2024-10-15 21:52:34 +0530

    add product page ui

---
+++ frontend/src/components/admin/image-upload.jsx
@@ -0,0 +1,86 @@
+import React, { useRef } from "react";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { FileIcon, UploadCloudIcon, XIcon } from "lucide-react";
+import { Button } from "../ui/button";
+
+const ProductImageUpload = ({
+  imageFile,
+  setImageFile,
+  uploadedImageUrl,
+  setUploadedImageUrl,
+}) => {
+  const inputRef = useRef(null);
+
+  const handleImageFileChange = (e) => {
+    const selectedFile = e.target.files?.[0];
+    if (selectedFile) setImageFile(selectedFile);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const droppedFile = e.dataTransfer.files?.[0];
+
+    if (droppedFile) setImageFile(droppedFile);
+  };
+
+  const handleRemoveImage = () => {
+    setImageFile(null);
+    if (inputRef.current) {
+      inputRef.current.value = "";
+    }
+  };
+
+  return (
+    <div className="w-full max-w-md mx-auto mt-2">
+      <Label className="text-base font-semibold mb-2 block">Upload Image</Label>
+      <div
+        className="border-2 border-dashed rounded-lg p-4"
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
+      >
+        <Input
+          id="image-upload"
+          type="file"
+          className="hidden"
+          ref={inputRef}
+          onChange={handleImageFileChange}
+        />
+
+        {!imageFile ? (
+          <Label
+            htmlFor="image-upload"
+            className="flex flex-col items-center justify-center h-32 cursor-pointer"
+          >
+            <UploadCloudIcon className="w-10 h-10 text-muted-foreground mb-2" />
+            <span className="text-muted-foreground">
+              Drag & Drop or Click to upload image
+            </span>
+          </Label>
+        ) : (
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <FileIcon className="w-8 h-8 text-primary mr-2" />
+            </div>
+            <p className="text-sm font-medium">{imageFile.name}</p>
+            <Button
+              variant="ghost"
+              size="icon"
+              className="text-muted-foreground hover:text-foreground"
+              onClick={handleRemoveImage}
+            >
+              <XIcon className="w-4 h-4" />
+              <span className="sr-only">Remove File</span>
+            </Button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ProductImageUpload;
--- frontend/src/pages/admin/Products.jsx
+++ frontend/src/pages/admin/Products.jsx
@@ -1,7 +1,74 @@
-import React from "react";
+import ProductImageUpload from "@/components/admin/image-upload";
+import CommonForm from "@/components/common/form";
+import { addProductFormElements } from "@/components/config";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import React, { useState } from "react";
+
+const initialState = {
+  image: null,
+  title: "",
+  description: "",
+  category: "",
+  brand: "",
+  price: "",
+  salePrice: "",
+  totalStock: "",
+  averageReview: 0,
+};
 
 const Products = () => {
-  return <div>Products</div>;
+  const [openCreateProductsDialog, setOpenCreateProductDialog] =
+    useState(false);
+  const [formData, setFormData] = useState(initialState);
+  const [imageFile, setImageFile] = useState(null);
+  const [uploadedImageUrl, setUploadedImageUrl] = useState("");
+
+  // HANDLE SUBMIT
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <div className="mb-5 flex justify-end">
+        <Button onClick={() => setOpenCreateProductDialog(true)}>
+          Add New Product
+        </Button>
+      </div>
+      <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4"></div>
+      <Sheet
+        open={openCreateProductsDialog}
+        onOpenChange={() => setOpenCreateProductDialog(false)}
+      >
+        <SheetContent side="right" className="overflow-auto">
+          <SheetHeader>
+            <SheetTitle>Add New Product</SheetTitle>
+          </SheetHeader>
+          <ProductImageUpload
+            imageFile={imageFile}
+            setImageFile={setImageFile}
+            uploadedImageUrl={uploadedImageUrl}
+            setUploadedImageUrl={setUploadedImageUrl}
+          />
+          <div className="py-6">
+            <CommonForm
+              formControls={addProductFormElements}
+              onSubmit={onSubmit}
+              buttonText={"Add"}
+              formData={formData}
+              setFormData={setFormData}
+            />
+          </div>
+        </SheetContent>
+      </Sheet>
+    </>
+  );
 };
 
 export default Products;
