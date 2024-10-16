7fb77b4b15a90dfc1b88fa5bdd21c25a74dbe71d
MohitCode17 <mohit.codes17@gmail.com>
2024-10-16 21:33:26 +0530

    add product related controllers

--- backend/controllers/admin/product.controller.js
+++ backend/controllers/admin/product.controller.js
@@ -1,6 +1,7 @@
 import ErrorHandler from "../../middlewares/error.js";
 import { catchAsyncErrors } from "../../middlewares/catchAsyncError.js";
 import { cloudinaryUpload } from "../../helper/cloudinary-upload.js";
+import Product from "../../models/product.model.js";
 
 export const handleImageUpload = catchAsyncErrors(async (req, res, next) => {
   if (!req.files || Object.keys(req.files).length === 0)
@@ -17,14 +18,92 @@ export const handleImageUpload = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
-export const handleAddProduct = catchAsyncErrors(async (req, res, next) => {});
+export const handleAddProduct = catchAsyncErrors(async (req, res, next) => {
+  const {
+    image,
+    title,
+    description,
+    category,
+    brand,
+    price,
+    salePrice,
+    totalStock,
+    averageReview,
+  } = req.body;
 
-export const handleEditProduct = catchAsyncErrors(async (req, res, next) => {});
+  console.log(averageReview, "averageReview");
 
-export const handleDeleteProduct = catchAsyncErrors(
-  async (req, res, next) => {}
-);
+  const newProduct = await Product.create({
+    image,
+    title,
+    description,
+    category,
+    brand,
+    price,
+    salePrice,
+    totalStock,
+    averageReview,
+  });
 
-export const fetchAllProducts = catchAsyncErrors(async (req, res, next) => {});
+  res.status(201).json({
+    success: true,
+    newProduct,
+  });
+});
 
-export const fetchProduct = catchAsyncErrors(async (req, res, next) => {});
+export const handleEditProduct = catchAsyncErrors(async (req, res, next) => {
+  const { id } = req.params;
+  const {
+    image,
+    title,
+    description,
+    category,
+    brand,
+    price,
+    salePrice,
+    totalStock,
+    averageReview,
+  } = req.body;
+
+  const product = await Product.findById(id);
+
+  if (!product) return next(new ErrorHandler("No product found.", 404));
+
+  product.title = title || product.title;
+  product.description = description || product.description;
+  product.category = category || product.category;
+  product.brand = brand || product.brand;
+  product.price = price === "" ? 0 : price || product.price;
+  product.salePrice = salePrice === "" ? 0 : salePrice || product.salePrice;
+  product.totalStock = totalStock || product.totalStock;
+  product.averageReview = averageReview || product.averageReview;
+  product.image = image || product.image;
+
+  await product.save();
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
+
+export const handleDeleteProduct = catchAsyncErrors(async (req, res, next) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndDelete(id);
+
+  if (!product) return next(new ErrorHandler("No product found", 404));
+
+  res.status(200).json({
+    success: true,
+    message: "Product deleted successfully",
+  });
+});
+
+export const fetchAllProducts = catchAsyncErrors(async (req, res, next) => {
+  const products = await Product.find({});
+
+  res.status(200).json({
+    success: true,
+    products,
+  });
+});
