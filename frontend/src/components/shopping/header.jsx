b56ac2ffdf2377f2a11aeb7b71d70d0a3775aa40
MohitCode17 <mohit.codes17@gmail.com>
2024-10-21 22:42:42 +0530

    add shopping view header ui

--- frontend/src/components/shopping/header.jsx
+++ frontend/src/components/shopping/header.jsx
@@ -1,7 +1,126 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { GiShoppingBag } from "react-icons/gi";
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import { Button } from "../ui/button";
+import { LogOut, Menu, ShoppingCart, UserCog } from "lucide-react";
+import { shoppingViewHeaderMenuItems } from "../config";
+import { Label } from "../ui/label";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Avatar, AvatarFallback } from "../ui/avatar";
+import { useDispatch, useSelector } from "react-redux";
+import { logoutUser } from "@/store/authSlice";
+
+const MenuItems = () => {
+  const navigate = useNavigate();
+
+  return (
+    <nav className="flex flex-col mb-3 lg:mb-0 lg:items-center lg:flex-row gap-6">
+      {shoppingViewHeaderMenuItems.map((menuItem) => (
+        <Label
+          key={menuItem.id}
+          className="text-sm font-medium cursor-pointer"
+          onClick={() => navigate(menuItem.path)}
+        >
+          {menuItem.label}
+        </Label>
+      ))}
+    </nav>
+  );
+};
+
+const HeaderRightContent = () => {
+  const { user } = useSelector((state) => state.auth);
+  const [openCartSheet, setOpenCartSheet] = useState(false);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logoutUser());
+  };
+
+  return (
+    <div className="flex lg:items-center lg:flex-row flex-col gap-4">
+      <Sheet open={openCartSheet} onOpenChange={() => setOpenCartSheet(false)}>
+        <Button
+          onClick={() => setOpenCartSheet(true)}
+          variant="outline"
+          size="icon"
+          className="relative"
+        >
+          <ShoppingCart className="h-6 w-6" />
+          <span className="absolute top-[-10px] right-[1px] font-bold text-base">
+            3
+          </span>
+          <span className="sr-only">User cart</span>
+        </Button>
+      </Sheet>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Avatar className="bg-black">
+            <AvatarFallback className="bg-black text-white font-extrabold">
+              {user?.userName[0].toUpperCase()}
+            </AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent side="bottom" className="w-56">
+          <DropdownMenuLabel>Logged in as {user?.userName}</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={() => navigate("/shop/account")}>
+            <UserCog className="mr-2 h-4 w-4" />
+            Account
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={handleLogout}>
+            <LogOut className="mr-2 h-4 w-4" />
+            Logout
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
 
 const ShoppingHeader = () => {
-  return <div>ShoppingHeader</div>;
+  return (
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="flex h-16 items-center justify-between px-4 md:px-6">
+        {/* LOGO */}
+        <Link to={"/shop/home"} className="flex items-center gap-1">
+          <GiShoppingBag className="h-6 w-6" />
+          <span className="font-bold font-mono text-2xl mt-1">Luxora</span>
+        </Link>
+
+        {/* MOBILE DEVICE NAVBAR MENUS */}
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon" className="lg:hidden">
+              <Menu className="h-6 w-6" />
+              <span className="sr-only">Toggle header menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left" className="w-full max-w-xs">
+            <MenuItems />
+            <HeaderRightContent />
+          </SheetContent>
+        </Sheet>
+        <div className="hidden lg:block">
+          <MenuItems />
+        </div>
+        <div className="hidden lg:block">
+          <HeaderRightContent />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default ShoppingHeader;
